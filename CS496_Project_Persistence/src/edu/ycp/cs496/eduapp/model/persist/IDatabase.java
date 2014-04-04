e3d86b0ed71d4da4471087eed2f8caa772a98a20
acummins <acummins@kec119-07.ycp.edu>
2014-04-04 14:51:08 -0400

    Added Create Account Backend

--- CS496_Project_Persistence/src/edu/ycp/cs496/eduapp/model/persist/IDatabase.java
+++ CS496_Project_Persistence/src/edu/ycp/cs496/eduapp/model/persist/IDatabase.java
@@ -1,7 +1,11 @@
 package edu.ycp.cs496.eduapp.model.persist;
 
+import edu.ycp.cs496.eduapp.model.User;
+
 public interface IDatabase {
 
 	boolean authenticateUser(String user, String pass);
+
+	boolean createAccount(User inUser,boolean isProf);
 
 }
