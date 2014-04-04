6a9511ecaa4fc817747845963717c581402d0deb
aoduyebo <aoduyebo@kec119-24.ycp.edu>
2014-04-04 14:58:42 -0400

    Added Login view

--- CS496_Project_Webservice/src/edu/ycp/cs496/eduapp/webapp/servlets/Login.java
+++ CS496_Project_Webservice/src/edu/ycp/cs496/eduapp/webapp/servlets/Login.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Home extends HttpServlet {
+public class Login extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		resp.setContentType("application/json");
 		resp.setStatus(HttpServletResponse.SC_OK);
-		resp.getWriter().println("\"hello\"");
+		req.getRequestDispatcher("/_view/Home.jsp").forward(req,resp);
 	}
 }
