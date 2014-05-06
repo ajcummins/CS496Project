d472bfa80609b570c971e84f0cf51993db4cae54
Mr_E <Mr_E@172.31.5.40>
2014-05-06 16:10:33 -0400

    error fix

--- CS496_Project_Controllers/src/edu/ycp/cs496/eduapp/model/controllers/EditCourseById.java
+++ CS496_Project_Controllers/src/edu/ycp/cs496/eduapp/model/controllers/EditCourseById.java
@@ -5,7 +5,7 @@ import edu.ycp.cs496.eduapp.model.persist.DatabaseProvider;
 import edu.ycp.cs496.eduapp.model.persist.IDatabase;
 
 public class EditCourseById {
-	public Course editCourseByCode(String courseCode) {
+	public Course editCourseByCode(Course courseCode) {
 		IDatabase db = DatabaseProvider.getInstance();
 		return db.editCourseByCode(courseCode);
 	}
